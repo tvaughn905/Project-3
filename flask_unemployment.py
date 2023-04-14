dc9fb87b6ab4230aa623a283002c5e9ab079a4b7
Chris Lynch <lynchch4@gmail.com>
2023-04-13 21:22:23 -0400

    Update flask_unemployment.py

--- flask_unemployment.py
+++ flask_unemployment.py
@@ -1,35 +1,33 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    return {"hello": "world"}
-
-if __name__ == '__main__':
-    app.run(debug=True)
+import psycopg2
+from flask import Flask, render_template
 
 # Previous imports remain...
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+##from flask_sqlalchemy import SQLAlchemy
+##from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@localhost:5432/cars_api"
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+con = psycopg2.connect(database="cars_api", user="postgres", password="postgres", host = "127.0.0.1", port = "5000")
+cursor = con.cursor()
 
-class CarsModel(db.Model):
-    __tablename__ = 'cars'
+@app.route('/', methods=['post', 'get'])
+def testing():
+    cursor.execute("select * from car")
+    result = cursor.fetchall
+    return render_template("testing", data = result)
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String())
-    model = db.Column(db.String())
-    doors = db.Column(db.Integer())
+#class CarsModel(db.Model):
+#    __tablename__ = 'cars'#
 
-    def __init__(self, name, model, doors):
-        self.name = name
-        self.model = model
-        self.doors = doors
+#    id = db.Column(db.Integer, primary_key=True)
+#    name = db.Column(db.String())
+#    model = db.Column(db.String())
+ #   doors = db.Column(db.Integer())
 
-    def __repr__(self):
-        return f"<Car {self.name}>"
+ #   def __init__(self, name, model, doors):
+ #       self.name = name
+ #       self.model = model
+ #       self.doors = doors
+
+    
+if __name__ == '__main__':
+    app.run(debug=True)
