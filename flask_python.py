7b59d3c2bf166b1fd11ae2a6585ab23e2550fa4d
Chris Lynch <lynchch4@gmail.com>
2023-04-16 15:44:06 -0400

    flask added ehtnicity data

--- flask_python.py
+++ flask_python.py
@@ -21,5 +21,15 @@ def index():
     conn.close()
     return Response(json.dumps(books),  mimetype='application/json')
 
+@app.route('/ethnicity')
+def index():
+    conn =psycopg2.connect(database="flask_df", user="flask_df_user", password="DF4LP1UcZJt3AN4cUW9hrbfp2p4FtWL3", host="dpg-cgtiqjl269vbmeuj26cg-a.oregon-postgres.render.com", port="5432")
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM ethnicity;')
+    books = cur.fetchall()
+    cur.close()
+    conn.close()
+    return Response(json.dumps(books),  mimetype='application/json')
+
 if __name__ == '__main__':
     app.run(debug=True)
