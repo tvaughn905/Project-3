7d654f975968b0f50453ddcadb69db8ad8bc3528
Chris Lynch <lynchch4@gmail.com>
2023-04-15 21:44:10 -0400

    Update flask_postgres.py

--- flask_postgres.py
+++ flask_postgres.py
@@ -3,14 +3,14 @@ from flask import Flask#,render_template
 
 
 app = Flask(__name__)
-con = psycopg2.connect(database="Movie", user="postgres", password="postgres", host="127.0.0.1", port="5000")##
+con = psycopg2.connect(database="Movie_Rentals", user="postgres", password="postgres", host="127.0.0.1", port="5432")##
 cursor = con.cursor()
 
-@app.route('/', methods =['post', 'get'])
+##@app.route('/')##, methods =['post', 'get']
 def testing():
     cursor.execute("select * from actor")
     result = cursor.fetchall
-    print(result)
+    return str(result)
     
     ##return render_template("testing.csv", data=result)
 
