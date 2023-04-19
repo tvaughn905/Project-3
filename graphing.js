667081b4bebac2fde4a878562ec56c24ec24128e
Chris Lynch <lynchch4@gmail.com>
2023-04-19 19:25:43 -0400

    update

--- graphing.js
+++ graphing.js
@@ -6,6 +6,18 @@ const data = d3.json(url).then(function(data) {console.log(data)});
 const data2 = d3.json(url2).then(function(data2) {console.log(data2)});
 var industry_list = []
 var fy14_list = []
+var fy15_list = []
+var fy16_list = []
+var fy17_list = []
+var fy18_list = []
+var fy19_list = []
+var fy20_list = []
+var fy21_list = []
+var date_list = []
+var white_list = []
+var black_list = []
+var asian_list = []
+var hisp_list = []
 
 function start() {
 
@@ -30,32 +42,108 @@ function start() {
     console.log(industry_list)
     console.log(fy14_list)
     document.addEventListener('DOMContentLoaded', function () {
-        const chart = Highcharts.chart('bar', {
-            chart: {
-                type: 'bar'
-            },
-            title: {
-                text: 'Unemployment by Industry Type'
-            },
-            xAxis: {
-                categories: industry_list
-            },
-            yAxis: {
+        d3.json(url).then(function(data) {
+            for (let i = 0; i < data.length; i++)    
+                fy14_list.push(data[i].fy14)
+            for (let i = 0; i < data.length; i++)    
+                fy15_list.push(data[i].fy15)
+            for (let i = 0; i < data.length; i++)    
+                fy16_list.push(data[i].fy16)
+            for (let i = 0; i < data.length; i++)
+                fy17_list.push(data[i].fy17)  
+            for (let i = 0; i < data.length; i++)    
+                fy18_list.push(data[i].fy18)       
+            for (let i = 0; i < data.length; i++)    
+                fy19_list.push(data[i].fy19)
+            for (let i = 0; i < data.length; i++)    
+                fy20_list.push(data[i].fy20)
+            for (let i = 0; i < data.length; i++)    
+                fy21_list.push(data[i].fy21)        
+                const chart = Highcharts.chart('bar', {
+                chart: {
+                    type: 'bar'
+                },
                 title: {
-                    text: 'Unemployment in 1000s'
-                }
-            },
-            series: [{
-                name: "FY14",
-                data: [1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5]//fy14_list
-            }, {
-                name: "FY15",
-                data: [1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5]//fy14_list
-            }]
+                    text: 'Unemployment by Industry Type'
+                },
+                xAxis: {
+                    categories: industry_list
+                },
+                yAxis: {
+                    title: {
+                        text: 'Unemployment in 1000s'
+                    }
+                },
+                series: [{
+                    name: "FY15",   
+                    data: fy15_list
+                }, {
+                    name: "FY16",   
+                    data: fy16_list
+                }, {
+                    name: "FY17",   
+                    data: fy17_list
+                }, {
+                    name: "FY18",   
+                    data: fy18_list
+                }, {
+                    name: "FY19",   
+                    data: fy19_list
+                }, {
+                    name: "FY20",   
+                    data: fy20_list
+                }, {
+                    name: "FY21",   
+                    data: fy21_list
+                }]
+            });
         });
     });
-};
 
+    document.addEventListener('DOMContentLoaded', function () {
+        d3.json(url).then(function(data) {
+            for (let i = 0; i < data.length; i++)    
+                date_list.push(data[i].fy14)
+            for (let i = 0; i < data.length; i++)    
+                white_list.push(data[i].fy15)
+            for (let i = 0; i < data.length; i++)    
+                asian_list.push(data[i].fy16)
+            for (let i = 0; i < data.length; i++)
+                black_list.push(data[i].fy17)  
+            for (let i = 0; i < data.length; i++)    
+                hisp_list.push(data[i].fy18)   
+            const chart = Highcharts.chart('line', {
+                chart: {
+                    type: 'line'
+                },
+                title: {
+                    text: '??????????????'
+                },
+                xAxis: {
+                    categories: ['Apples', 'Bananas', 'Oranges']
+                },
+                yAxis: {
+                    title: {
+                        text: 'Fruit eaten'
+                    }
+                },
+                series: [{
+                    name: 'Asian',
+                    data: [1, 0, 4]
+                }, {
+                    name: 'Black',
+                    data: [5, 7, 3]
+                }, {
+                    name: 'Hispanic',
+                    data: [5, 7, 3]
+                }, {
+                    name: 'White',
+                    data: [5, 7, 3]
+                }]
+            })
+        })    
+    })
+};
 ///function graphs(set) {
     
 
@@ -120,4 +208,3 @@ document.addEventListener('DOMContentLoaded', function () {
         }]
     });
 });
-
